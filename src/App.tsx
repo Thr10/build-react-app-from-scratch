6f3ba2256b1b107d82b327c0e257e2b2177e9bd6
Thr10 <359366380@qq.com>
2023-03-06 16:03:45 +0800

    config eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
 // App.tsx
-import React from "react";
-export const App: React.FC = () => <h1>Hello React  aa</h1>;
+import React from 'react';
+
+const App: React.FC = () => <h1>Hello React  aa</h1>;
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,63 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {DefinePlugin} = require('webpack');
+const { DefinePlugin } = require('webpack');
 
 module.exports = {
-    entry: './src/index.tsx',
-    output: {
-        filename: 'bundle.[contenthash].js',
-        path: path.resolve(__dirname, './dist'),
-        clean: true
-    },
-    mode: 'none',
-    resolve: {
-        extensions: ['.tsx', '.ts', '.jsx', '.js'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
-                ]
-            },
-            {
-                test: /\.hbs$/,
-                use: [
-                    'handlebars-loader'
-                ]
-            },
-            {
-                test: /\.(js|ts)x?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            }
-        ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'styles.[contenthash].css',
-        }),
-        new HtmlWebpackPlugin({
-            title: 'Hello world',
-            filename: 'index.html',
-            template: 'public/index.hbs',
-            description: 'Some description',
-        }),
-        new DefinePlugin({
-            'process.env' : {
-                NODE_ENV: process.env.NODE_ENV
-            }
-        })
-    ],
-    devServer: {
-        static: {
-          directory: path.join(__dirname, 'dist'),
+  entry: './src/index.tsx',
+  output: {
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, './dist'),
+    clean: true,
+  },
+  mode: 'none',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader',
+        ],
+      },
+      {
+        test: /\.hbs$/,
+        use: [
+          'handlebars-loader',
+        ],
+      },
+      {
+        test: /\.(js|ts)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
         },
-        compress: true,
-        port: 3333,
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.[contenthash].css',
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Hello world',
+      filename: 'index.html',
+      template: 'public/index.hbs',
+      description: 'Some description',
+    }),
+    new DefinePlugin({
+      'process.env': {
+        NODE_ENV: process.env.NODE_ENV,
+      },
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
     },
-}
+    compress: true,
+    port: 3333,
+  },
+};
