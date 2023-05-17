809b14e122f22595d6d79744037b3b635a290be3
Thr10 <359366380@qq.com>
2023-05-17 16:03:05 +0800

    项目添加redux作为全局状态管理工具

--- src/Pages/Bar/index.tsx
+++ src/Pages/Bar/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { message } from 'antd';
+import { useAppSelector, useAppDispatch } from '../../store/storeHooks';
+import { decrement, increment } from '../../store/slice/counterSlice';
 import { register } from '../../api/user';
 
 const Bar = () => {
+  // 测试redux
+  const count = useAppSelector((state) => { return state.counter.value; });
+  const dispatch = useAppDispatch();
+  // 网络请求测试
   const handleClick = async () => {
     try {
       const res = await register({
@@ -17,7 +23,23 @@ const Bar = () => {
   };
   return (
     <div>
-      Bar
+      <div>
+        <button
+          type="button"
+          aria-label="Increment value"
+          onClick={() => { return dispatch(increment()); }}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          type="button"
+          aria-label="Decrement value"
+          onClick={() => { return dispatch(decrement()); }}
+        >
+          Decrement
+        </button>
+      </div>
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import store from './store';
 import App from './App';
 
 const rootDom = document.getElementById('root');
 const container = createRoot(rootDom!);
-container.render(<App />);
+container.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+);
